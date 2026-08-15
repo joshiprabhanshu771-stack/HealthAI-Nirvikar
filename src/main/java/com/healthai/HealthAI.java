538f36f02a84b10f385601a021f2a585565bd4c8
Prabhanshu <joshiprabhanshu771@gmail.com>
2026-08-15 23:44:02 +0530

    Initial commit - HealthAI

---
+++ src/main/java/com/healthai/HealthAI.java
@@ -0,0 +1,12 @@
+package com.healthai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthAI {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthAI.class, args);
+    }
+}
